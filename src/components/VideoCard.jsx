c707eb6f85f6a2daf48cf62b1989b62112003b02
Ramzi Hidan <ramzihaidan9@gmail.com>
2025-01-26 17:02:51 +0300

    adding Channel Card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,9 +4,7 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material'; 
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl } from '../utils/constants';
 
-
 const VideoCard = ({ video: { id:{videoId}, snippet }}) => {
-    console.log(videoId, snippet)
   return (
     <Card sx={{ width: { md:'320px', xs:'100%' },
      boxShadow: 'none', borderRadius: 0 }} >
@@ -18,18 +16,18 @@ const VideoCard = ({ video: { id:{videoId}, snippet }}) => {
             />
         </Link>
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} >
-        <Link to={videoId? `/video/${videoId}` : demoVideoUrl }>
-            <Typography variant='subtitle1' fontWeight="bold" color="#FFF"  >
-                {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60) }
-            </Typography>
-        </Link>
-        
-        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl }>
-            <Typography variant='subtitle2' fontWeight="bold" color="gray"  >
-                {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60) }
-                <CheckCircle sx={{fontSize:12 , color:'gray' , ml:'5px'}} />
-            </Typography>
-        </Link>
+          <Link to={videoId? `/video/${videoId}` : demoVideoUrl }>
+              <Typography variant='subtitle1' fontWeight="bold" color="#FFF"  >
+                  {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60) }
+              </Typography>
+          </Link>
+          
+          <Link to={`/channel/${snippet?.channelId}`}>
+              <Typography variant='subtitle2' fontWeight="bold" color="gray"  >
+                  {snippet?.channelTitle || demoChannelTitle}
+                  <CheckCircle sx={{fontSize:12 , color:'gray' , ml:'5px'}} />
+              </Typography>
+          </Link>
         </CardContent>
     </Card>
   )
